744e4064e7e40dbb64ed0990148972a30bbcad4b
kaczooor <68024536+kaczooor@users.noreply.github.com>
2021-03-19 18:41:02 +0100

    Add new files

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+description "Forza Horizon 4 Minimap"
+author "Akkariin"
+url "https://www.zerodream.net/"
+version "1.2"
+
+client_script 'client.lua'
+
+files {
+	'hud.html',
+	'mapzoomdata.meta',
+    'pausemenu.xml'
+}
+
+ui_page "hud.html"
